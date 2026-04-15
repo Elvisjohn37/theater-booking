b0567692df547edd42365581b07a7ebf493316fc
Elvis <elvisjohn.cayetano@stremlineverify.com>
2026-04-15 19:05:53 +0800

    Added login form

--- frontend/app/actions.ts
+++ frontend/app/actions.ts
@@ -3,25 +3,25 @@
 export async function loginAction(formData: FormData) {
     const username = formData.get("username")
     const password = formData.get("password")
+    return { success: false, message: "Login failed" }
+    // const response = await fetch(
+    //     `${process.env.NEXT_PUBLIC_PHP_API_URL}/auth/login.php`,
+    //     {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({ username, password }),
+    //     },
+    // )
 
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_PHP_API_URL}/auth/login.php`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ username, password }),
-        },
-    )
+    // const data = await response.json()
 
-    const data = await response.json()
-
-    if (response.ok) {
-        // Handle success (e.g., set cookies or redirect)
-        return { success: true, message: data.message }
-    } else {
-        // Handle error
-        return { success: false, message: data.message || "Login failed" }
-    }
+    // if (response.ok) {
+    //     // Handle success (e.g., set cookies or redirect)
+    //     return { success: true, message: data.message }
+    // } else {
+    //     // Handle error
+    //     return { success: false, message: data.message || "Login failed" }
+    // }
 }
